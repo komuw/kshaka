8948c44d78deb588f3266b85f766339652285737
komuW <komuw05@gmail.com>
2018-03-27 19:36:08 +0300

    add locks

--- kshaka.go
+++ kshaka.go
@@ -5,7 +5,7 @@ It's name is derived from the Kenyan hip hop group, Kalamashaka.
 "CASPaxos is a replicated state machine (RSM) protocol. Unlike Raft and Multi-Paxos,
 it doesn’t use leader election and log replication, thus avoiding associated complexity.
 Its symmetric peer-to-peer approach achieves optimal commit latency in wide-area networks
-and doesn’t cause transient unavailability when any ⌊N−1⌋ of N nodes crash." - [The CASPaxos whitepaper](https://github.com/rystsov/caspaxos/blob/master/latex/caspaxos.pdf)
+and doesn’t cause transient unavailability when any [N−1] of N nodes crash." - [The CASPaxos whitepaper](https://github.com/rystsov/caspaxos/blob/master/latex/caspaxos.pdf)
 
 TODO: add system design here.
 */
