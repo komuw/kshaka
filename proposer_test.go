cf4c7a573ce07b8a23afe4cb281fec66e310dd2c
komuW <komuw05@gmail.com>
2018-04-04 15:26:36 +0300

    test

--- proposer_test.go
+++ proposer_test.go
@@ -6,8 +6,8 @@ import (
 )
 
 func Test_proposer_Propose(t *testing.T) {
-	kv2 := map[string][]byte{"": []byte("")}
-	acceptorStore := &InmemStore{kv: kv2}
+	kv := map[string][]byte{"": []byte("")}
+	acceptorStore := &InmemStore{kv: kv}
 
 	var readFunc ChangeFunction = func(key []byte, current []byte) ([]byte, error) {
 		return current, nil
@@ -37,28 +37,21 @@ func Test_proposer_Propose(t *testing.T) {
 			wantErr: true,
 		},
 		{name: "two acceptors",
-			p:       proposer{id: 1, ballot: ballot{Counter: 1, ProposerID: 1}, acceptors: []*acceptor{&acceptor{id: 1}, &acceptor{id: 2}}},
+			p:       proposer{id: 1, ballot: ballot{Counter: 1, ProposerID: 1}, acceptors: []*acceptor{&acceptor{id: 1, stateStore: acceptorStore}, &acceptor{id: 2, stateStore: acceptorStore}}},
 			args:    args{key: []byte("foo"), changeFunc: readFunc},
 			want:    nil,
 			wantErr: true,
 		},
 		{name: "enough acceptors readFunc",
-			p:       proposer{id: 1, ballot: ballot{Counter: 1, ProposerID: 1}, acceptors: []*acceptor{&acceptor{id: 1}, &acceptor{id: 2}, &acceptor{id: 3}, &acceptor{id: 4}}},
+			p:       proposer{id: 1, ballot: ballot{Counter: 1, ProposerID: 1}, acceptors: []*acceptor{&acceptor{id: 1, stateStore: acceptorStore}, &acceptor{id: 2, stateStore: acceptorStore}, &acceptor{id: 3, stateStore: acceptorStore}, &acceptor{id: 4, stateStore: acceptorStore}}},
 			args:    args{key: []byte("foo"), changeFunc: readFunc},
 			want:    nil,
 			wantErr: false,
 		},
 		{name: "enough acceptors setFunc",
-			p:       proposer{id: 1, ballot: ballot{Counter: 1, ProposerID: 1}, acceptors: []*acceptor{&acceptor{id: 1}, &acceptor{id: 2}, &acceptor{id: 3}, &acceptor{id: 4}}},
+			p:       proposer{id: 1, ballot: ballot{Counter: 1, ProposerID: 1}, acceptors: []*acceptor{&acceptor{id: 1, stateStore: acceptorStore}, &acceptor{id: 2, stateStore: acceptorStore}, &acceptor{id: 3, stateStore: acceptorStore}, &acceptor{id: 4, stateStore: acceptorStore}}},
 			args:    args{key: []byte("foo"), changeFunc: setFunc([]byte("stephen"), []byte("hawking"))},
 			want:    []byte("hawking"),
-			wantErr: false,
-		},
-
-		{name: "enough acceptors setFunc. acceptor with own stores",
-			p:       proposer{id: 1, ballot: ballot{Counter: 1, ProposerID: 1}, acceptors: []*acceptor{&acceptor{id: 1, stateStore: acceptorStore}, &acceptor{id: 2, stateStore: acceptorStore}, &acceptor{id: 3, stateStore: acceptorStore}, &acceptor{id: 4, stateStore: acceptorStore}}},
-			args:    args{key: []byte("foo"), changeFunc: setFunc([]byte("bob"), []byte("marley"))},
-			want:    []byte("marley"),
 			wantErr: false,
 		},
 	}
