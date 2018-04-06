66fb790f5164d7ecc767599ef667eb458c9a6f58
komuW <komuw05@gmail.com>
2018-04-06 18:23:18 +0300

    add newNode test

--- node_test.go
+++ node_test.go
@@ -27,3 +27,40 @@ func TestNode_incBallot(t *testing.T) {
 		})
 	}
 }
+
+func TestNewNode(t *testing.T) {
+	kv := map[string][]byte{"": []byte("")}
+	store := &InmemStore{kv: kv}
+
+	type args struct {
+		store StableStore
+		nodes []*Node
+	}
+	tests := []struct {
+		name        string
+		args        args
+		numberNodes int
+	}{
+		{name: "no nodes supplied",
+			args:        args{store: store},
+			numberNodes: 1},
+		{name: "1 node supplied",
+			args:        args{store: store, nodes: []*Node{NewNode(store)}},
+			numberNodes: 2},
+
+		{name: "7 node supplied",
+			args:        args{store: store, nodes: []*Node{NewNode(store), NewNode(store), NewNode(store), NewNode(store), NewNode(store), NewNode(store), NewNode(store)}},
+			numberNodes: 8},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			n := NewNode(tt.args.store, tt.args.nodes...)
+			numNodes := len(n.nodes)
+
+			if numNodes != tt.numberNodes {
+				t.Errorf("\n NewNode \nnumNodes= %#+v, \nwanted = %#+v", numNodes, tt.numberNodes)
+			}
+
+		})
+	}
+}
