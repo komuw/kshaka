d29bbd464e6af193b1aa825d8804aa412faf40ad
komu <komuw05@gmail.com>
2018-04-08 12:25:15 +0300

    improve docs

--- node.go
+++ node.go
@@ -19,15 +19,12 @@ Example usage:
 	)
 
 	func main() {
-
-		fmt.Println("1")
 		// create a store that will be used. Ideally it should be a disk persisted store.
 		// any store that implements hashicorp/raft StableStore interface will suffice
 		boltStore, err := raftboltdb.NewBoltStore("/tmp/bolt.db")
 		if err != nil {
 			panic(err)
 		}
-		fmt.Println("2")
 
 		// The function that will be applied by CASPaxos. This will be applied to the
 		// current value stored under the key passed into the Propose method of the proposer.
@@ -36,31 +33,30 @@ Example usage:
 				return val, nil
 			}
 		}
-		fmt.Println("3")
 
-		// create a Node with a list of additional nodes.
-		// number of nodes needed for quorom ought to be >= 3
-		node1 := kshaka.NewNode(boltStore)
-		node2 := kshaka.NewNode(boltStore)
-		node3 := kshaka.NewNode(boltStore)
-		node4 := kshaka.NewNode(boltStore)
+		//create a Node with a list of additional nodes.
+		// number of nodes needed for quorom ought to be >= 3.
 
-		fmt.Println("4")
-		n := kshaka.NewNode(boltStore, node1, node2, node3, node4)
+		// Note that in this example; nodes are using the same store and are located in
+		// the same server/machine.
+		// In practice however, nodes ideally should be in different machines each with its own store.
+		node1 := kshaka.NewNode(1, boltStore)
+		node2 := kshaka.NewNode(2, boltStore)
+
+		node3 := kshaka.NewNode(3, boltStore, node1, node2)
 
 		key := []byte("name")
 		val := []byte("Masta-Ace")
 
-		fmt.Println("5")
 		// make a proposition;
 		// consensus via CASPaxos will happen and you will get the new state and any error back.
-		newstate, err := kshaka.Propose(n, key, setFunc(val))
+		newstate, err := kshaka.Propose(node3, key, setFunc(val))
 		if err != nil {
 			fmt.Printf("err: %v", err)
 		}
 		fmt.Printf("newstate: %v", newstate)
-		fmt.Println("6")
 	}
+
 
 TODO: add system design here.
 */
