94cffb55da0cceb0634ca86492d9978d4f66d9cd
komuW <komuw05@gmail.com>
2018-04-09 15:40:17 +0300

    guard against entering nodes more than once

--- node.go
+++ node.go
@@ -117,6 +117,7 @@ func NewNode(ID uint64, store StableStore, nodes ...*Node) *Node {
 func MingleNodes(nodes ...*Node) {
 	for _, n := range nodes {
 		// guard against adding same node twice
+		// TODO: fix this since it breaks down if len(nodes) > n.nodes
 		if len(n.nodes) < len(nodes) {
 			n.nodes = append(n.nodes, nodes...)
 		}
