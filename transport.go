69ba3503f0798f8f69736e7a2643766f753b7e71
komuW <komuw05@gmail.com>
2018-04-11 17:38:14 +0300

    update readme

---
+++ transport.go
@@ -0,0 +1,40 @@
+package kshaka
+
+// TODO: maybe add a transport interface
+// so that lib users can roll their own
+
+import "github.com/pkg/errors"
+
+type Args struct {
+	A, B int
+}
+
+type Quotient struct {
+	Quo, Rem int
+}
+
+type Arith int
+
+func (t *Arith) Multiply(args *Args, reply *int) error {
+	*reply = args.A * args.B
+	return nil
+}
+
+func (t *Arith) Divide(args *Args, quo *Quotient) error {
+	if args.B == 0 {
+		return errors.New("divide by zero")
+	}
+	quo.Quo = args.A / args.B
+	quo.Rem = args.A % args.B
+	return nil
+}
+
+
+arith := new(Arith)
+rpc.Register(arith)
+rpc.HandleHTTP()
+l, e := net.Listen("tcp", ":1234")
+if e != nil {
+	log.Fatal("listen error:", e)
+}
+go http.Serve(l, nil)
