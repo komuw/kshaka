7a14c7a3d97af4e51fff4966c088957bca13e746
komuW <komuw05@gmail.com>
2018-04-17 15:31:15 +0300

    handler err

--- examples/http_example/client.go
+++ examples/http_example/client.go
@@ -24,22 +24,26 @@ func main() {
 	url := "http://" + "127.0.0.1" + ":" + "15001" + "/propose"
 	propReqJSON, err := json.Marshal(propReq)
 	if err != nil {
-		panic(err)
+		fmt.Printf("\n err: %+v \n", err)
+		return
 	}
 	req, err := http.NewRequest("POST", url, bytes.NewBuffer(propReqJSON))
 	if err != nil {
-		panic(err)
+		fmt.Printf("\n err: %+v \n", err)
+		return
 	}
 	req.Header.Set("Content-Type", "application/json")
 	client := &http.Client{Timeout: time.Second * 3}
 	resp, err := client.Do(req)
 	if err != nil {
-		panic(err)
+		fmt.Printf("\n err: %+v \n", err)
+		return
 	}
 	defer resp.Body.Close()
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		panic(err)
+		fmt.Printf("\n err: %+v \n", err)
+		return
 	}
-	fmt.Println("client Propose response body::", body)
+	fmt.Println("client Propose response body::", string(body))
 }
