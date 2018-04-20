c96d286f05582b749d196ae3589de3d01c7398de
komuW <komuw05@gmail.com>
2018-04-20 17:07:53 +0300

    rm uneeded method

--- transport.go
+++ transport.go
@@ -2,6 +2,7 @@ package kshaka
 
 // Transport provides an interface for network transports
 // to allow kshaka/CASPaxos to communicate with other nodes.
+// An example is github.com/komuw/kshaka/httpTransport
 type Transport interface {
 	TransportPrepare(b Ballot, key []byte) (AcceptorState, error)
 	TransportAccept(b Ballot, key []byte, state []byte) (AcceptorState, error)
